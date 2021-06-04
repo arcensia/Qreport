652c227f959e2779ae45a0d71cec51ebf955f1af
arcensia <61192230+arcensia@users.noreply.github.com>
2021-06-04 19:58:41 +0900

    Add files via upload

--- Qreport.ino
+++ Qreport.ino
@@ -165,8 +165,8 @@ void loop(){
 #define AAction 4
 const double mygamma = 0.8;
 const int iterations = 10;
-//int initialStates[qSize] = {0, 1, 2, 3, 4, 5, 6, 7, 8};//<-상태를 항상 받는걸로 하자. 
-//int States = 0; //<-현재 state
+int initialStates[qSize] = {0, 1, 2, 3, 4, 5, 6, 7, 8};//<-상태를 항상 받는걸로 하자. 
+//int States = 0; //<-현재 state?
 // 0 장애물 감지x
 // 1 장애물 감지 후(기준점 30cm) -30 
 // 2 -20
@@ -206,7 +206,7 @@ void setup(){
     //Perform learning trials starting at all initial states.
     for(int j = 0; j < iterations/*10*/; j++){
         for(int i = 0; i < qSize; i++){
-            episode(initialStates[i]);//<-?
+            episode(initialStates[i]); //<-? 에피소드를 setup에서 Q를 완성시키는건가?
 		} // i
 	} // j
 
@@ -223,9 +223,9 @@ void setup(){
   Serial.println();
 }
 
-void loop(){ 
+void loop(){ //maximum에 따른 최종값을 실행
   forward();
-  int presentState = nowstates();
+  int primeState = nowstates();
   int newState; // <-?
 	//Perform tests, starting at all initial states.
 	for(int i = 0; i < qSize; i++){
@@ -233,16 +233,16 @@ void loop(){
     newState = 0;
 		do {
           newState = maximum(currentState, 1);
-          Serial.print(currentState);
-          Serial.print("->");
+          // Serial.print(currentState);
+          // Serial.print("->");
           currentState = newState;
-          ledOn(currentState+2); // <- 동작?
-        } while(currentState < AAction); //<- 마지막 기준점
-    Serial.println(5);
+          activateAction(currentState); // <- 동작?
+        } while(currentState < AAction); //<- 마지막 기준점을 어떻게 설정하지?
+    // Serial.println(5);
 	} // i  
 }
 
-void initialize(){//초기화
+void initialize(){//Q테이블 초기화
 	//srand((unsigned)time(0));
     for(int i = 0; i < qSize; i++){ // <- State
         for(int j = 0; j <qSize; j++){// <- Action
@@ -251,48 +251,49 @@ void initialize(){//초기화
 	} // i
 }
 
-void episode(int initialState){//초기 state에 대해
+void episode(int initialState){//초기 state에 대해    <-?????? 한 루프에 대한 끝 지점을 어떻게 집어넣지?// 각상황에 대해서
     currentState = initialState;
-    //Travel from state to state until goal state is reached.
-	do {
+    //Travel from state to state until goal state is reached. goal state == s[0]
+	do {//<- 왜 있는거지?//한 상황에 대해서 끝날 때 까지 실행한다. != 현 상태에서 계속 R값을 준다.
       chooseAnAction();//<- 여기서 currentState값이 바뀐다. 따라서 State가 0일 때 끝내는건 어떨까
-	} while(currentState == 5);//끝이 5야
+	} while(currentState == 0);//전진 일 때 실행?
 
-    //When currentState = 5, run through the set once more to
+    //When currentState = 0, run through the set once more to
     //for convergence.
-    for(int i = 0; i < qSize; i++){
-        chooseAnAction();
-	} // i
+  for(int i = 0; i < qSize; i++){//currentState마다 R값 Q에 집어넣는다.
+    chooseAnAction();
+  } // i
 }
-void chooseAnAction(){
+
+void chooseAnAction(){//방을 옮겼다는 느낌? //1회씩 진행된 값이 축적되어 Q값 쌓기
 	int possibleAction;
 
-    //Randomly choose a possible action connected to the current state.
-    possibleAction = getRandomAction(AAction, 0);
+  //Randomly choose a possible action connected to the current state.
+  possibleAction = getRandomAction(AAction, 0);//랜덤에서 R이 0보다 큰 Action을 가져와
 
-	if(R[currentState][possibleAction] >= 0){ //<- ?
-        Q[currentState][possibleAction] = reward(possibleAction);
-        currentState = possibleAction;
-        ledOn(currentState+2);
+	if(R[currentState][possibleAction] >= 0){ //<- R 값에서 +값이면 Q테이블에 적용 
+    Q[currentState][possibleAction] = reward(possibleAction);
+    currentState = possibleAction;
+    activateAction(currentState);//0보다 큰 값을 실행
 	}
 }
 
-int getRandomAction(int upperBound, int lowerBound){//최대 - 최소//upperCound == qsize == 9, lowerBound == 0
+int getRandomAction(int upperBound, int lowerBound){//최대 - 최소//upperCound == qsize == 9, lowerBound == 0 // 1초당 진행 (1회씩 진행)
 	int action;
 	int choiceIsValid = 0;
-    //Randomly choose a possible action connected to the current state.
-    do {
-        //srand((unsigned)time(NULL))
-        //Get a random value between 0 and 4.
-        action = lowerBound + rand() % AAction;
-        activateAction(action);
-        delay(1000);
-		if(R[currentState][action] > -1){//<-현재 상태에 대한 엑션
-            choiceIsValid = 1;
-		}
-    } while(choiceIsValid == 0);
 
-    return action;
+  //Randomly choose a possible action connected to the current state.
+  do {
+    //srand((unsigned)time(NULL))
+    //Get a random value between 0 and 4.
+    action = lowerBound + rand() % AAction;
+    //activateAction(action);
+    //delay(100); //<-여기서 문제가 생기나?
+    if(R[currentState][action] >= 0){//<-R테이블에서 0 이상이면 break
+      choiceIsValid = 1;
+    }
+  } while(choiceIsValid == 0);
+  return action;
 }
 
 // int nowstates(){
@@ -340,21 +341,21 @@ int maximum(int state, int returnIndexOnly){
   winner = 0;  
   do {
       foundNewWinner = 0;
-      for(int i = 0; i < qSize; i++){
-        if((i < winner) || (i > winner)){     //Avoid self-comparison.
+      for(int i = 0; i < qSize; i++){//state에 대한 최고값 추출
+        if((i < winner) || (i > winner)){     //Avoid self-comparison. // i != winner일 때
           if(Q[state][i] > Q[state][winner]){
             winner = i;
-            foundNewWinner = 1;
+            foundNewWinner = 1; // 최종값을 찾았다.
           }
         }
       } // i
 
-    if(foundNewWinner == 0){
+    if(foundNewWinner == 0){//못찾았다면 break
       done = 1; // break
     }
   } while(done == 0);
 
-	if(returnIndexOnly == 1){
+	if(returnIndexOnly == 1){//1인 경우 winner 리턴, 0인경우 R테이블에 대해 reward값을 더한다.
 		return winner;
 	}
   else{
@@ -362,7 +363,11 @@ int maximum(int state, int returnIndexOnly){
 	}
 }
 
-int reward(int action){
+// float reward(int action){
+//     return R[currentState][action] + (mygamma * maximum(action, 0));
+// }
+
+int reward(int action){//축적된 R과, maximum에서의 값 == R + Q(g*maximum)
     return R[currentState][action] + (mygamma * maximum(action, 0));
 }
 

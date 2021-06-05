efc8d0c738df9665ecfeedf879b8137a26ca4b35
arcensia <61192230+arcensia@users.noreply.github.com>
2021-06-05 20:13:35 +0900

    Add files via upload

--- Qreport.ino
+++ Qreport.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-//#define trigPin 10G;
+/////////////핀번호 셋/////////////////
 const int SERVO = 2;
 
 const int REN=5; //오른쪽En, PWM
@@ -15,7 +15,47 @@ const int trigPin = 13;
 
 Servo myServo;
 
+//////////////Q러닝////////////////////
+#define qSize 9
+#define AAction 4
+const double mygamma = 0.8;
+const int iterations = 10;
+int initialStates[qSize] = {0, 1, 2, 3, 4, 5, 6, 7, 8};//<-상태를 항상 받는걸로 하자. 
+// 0 장애물 감지x
+// 1 장애물 감지 후(기준점 30cm) -30 
+// 2 -20
+// 3 -10
+// 4 사이
+// 5  10
+// 6  20
+// 7  30
+// 8 장애물과의 거리(10cm이하)
+//학습에 대한 brake가 필요
+// 이걸 state에 따른 값이라고 해야할텐데.
+//Action 0. 좌회전, 1. 우회전, 2. 전진, 3. 후진
+int R[qSize][AAction] =  {{1, 1, 10, -3},
+                  			{-3, 3, -3, 0},
+                  			{-2, 2, -3, 0},
+                  			{-1, 1, -3, 0},
+                  			{1, 1, -3, 0},
+                  			{1, -1, -3, 0},
+                  			{2, -2, -3, 0},
+                  			{3, -3, -3, 0},
+                  			{-1, -1, -10, 3}
+                  			};
+
+int Q[qSize][AAction];
+void chooseAnAction(int shostate);
+int getRandomAction(int upperBound, int lowerBound, int gettState);
+float reward(int RcurrentState, int action);
+void episode(int initialState);
+int nowstates();
+int obscheck(int need);
+void activateAction(int Actnum);
+int maximum(int state, int returnIndexOnly);
+
 void setup(){
+  //pinset//
   Serial.begin(9600);
   myServo.attach(SERVO);
   pinMode(LEN, OUTPUT);
@@ -28,7 +68,45 @@ void setup(){
   pinMode(trigPin, OUTPUT);
   brake(LMC1,LMC2, LEN);
   brake(RMC1,RMC2, REN);  
+
+  //Qtable set//
+	int newState;
+	initialize();
+    //Perform learning trials starting at all initial states.
+  
+  for (int k = 0; k < sizeof(initialStates) / sizeof(int); k++){
+        episode(k);
+  }
+
+  Serial.println("done");
+
+  //Print out Q matrix.
+  for(int i = 0; i < qSize; i++){
+    for(int j = 0; j < AAction; j++){
+      Serial.print(Q[i][j]);
+      if(j < qSize - 1){
+        Serial.print(",");
+      }
+    } // j
+      Serial.println();
+  } // i
+  Serial.println();
 }
+
+// void setup(){
+//   Serial.begin(9600);
+//   myServo.attach(SERVO);
+//   pinMode(LEN, OUTPUT);
+//   pinMode(REN, OUTPUT);
+//   pinMode(LMC1, OUTPUT);
+//   pinMode(LMC2, OUTPUT);
+//   pinMode(RMC1, OUTPUT);
+//   pinMode(RMC2, OUTPUT);
+//   pinMode(echoPin, INPUT);
+//   pinMode(trigPin, OUTPUT);
+//   brake(LMC1,LMC2, LEN);
+//   brake(RMC1,RMC2, REN);  
+// }
 
 void forward (int MC1,int MC2, int EN){//전진함수
   digitalWrite(EN, LOW);
@@ -149,172 +227,228 @@ int limcm(){//장애물 인식 거리
   return 30;
 }
 
-void loop(){
-  // go(); //전지
-  // bbrake(); //정지
-  // leftturn(); // 좌회전
-  // rightturn(); // 우회전
-  // back(); // 후진
-  bbrake();
-  // checkUlt(readUlt(90)); // 초음파 체크
-  // checkServo(); // 서브모터 체크
+// void loop(){
+//   // go(); //전진
+//   // bbrake(); //정지
+//   // leftturn(); // 좌회전
+//   // rightturn(); // 우회전
+//   // back(); // 후진
+//   bbrake();
+//   // checkUlt(readUlt(90)); // 초음파 체크
+//   // checkServo(); // 서브모터 체크
+// }
+
+// //////////////Q러닝////////////////////
+// #define qSize 9
+// #define AAction 4
+// const double mygamma = 0.8;
+// const int iterations = 10;
+// int initialStates[qSize] = {0, 1, 2, 3, 4, 5, 6, 7, 8};//<-상태를 항상 받는걸로 하자. 
+// // 0 장애물 감지x
+// // 1 장애물 감지 후(기준점 30cm) -30 
+// // 2 -20
+// // 3 -10
+// // 4 사이
+// // 5  10
+// // 6  20
+// // 7  30
+// // 8 장애물과의 거리(10cm이하)
+// //학습에 대한 brake가 필요
+// // 이걸 state에 따른 값이라고 해야할텐데.
+// //Action 0. 좌회전, 1. 우회전, 2. 전진, 3. 후진
+// int R[qSize][AAction] =  {{1, 1, 10, -3},
+//                   			{-3, 3, -3, 0},
+//                   			{-2, 2, -3, 0},
+//                   			{-1, 1, -3, 0},
+//                   			{1, 1, -3, 0},
+//                   			{1, -1, -3, 0},
+//                   			{2, -2, -3, 0},
+//                   			{3, -3, -3, 0},
+//                   			{-1, -1, -10, 3}
+//                   			};
+
+// int Q[qSize][AAction];
+
+// void episode(int initialState);
+// void chooseAnAction();
+// int getRandomAction(int upperBound, int lowerBound);
+// void initialize();
+// int maximum(int state, int returnIndexOnly);
+// int reward(int action);
+// int nowstates();
+// void obscheck(int need);
+
+// void chooseAnAction(int shostate);
+// int getRandomAction(int upperBound, int lowerBound, int currentState);
+// float reward(int RcurrentState, int action);
+// int initialStates[9] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };//<-상태를 항상 받는걸로 하자. 
+// void episode(int initialState);
+// int nowstates();
+// int obscheck(int need);
+// void activateAction(int Actnum);
+// int maximum(int state, int returnIndexOnly);
+
+// void setup(){
+  
+//   Serial.begin(9600);
+//   myServo.attach(SERVO);
+//   pinMode(LEN, OUTPUT);
+//   pinMode(REN, OUTPUT);
+//   pinMode(LMC1, OUTPUT);
+//   pinMode(LMC2, OUTPUT);
+//   pinMode(RMC1, OUTPUT);
+//   pinMode(RMC2, OUTPUT);
+//   pinMode(echoPin, INPUT);
+//   pinMode(trigPin, OUTPUT);
+//   brake(LMC1,LMC2, LEN);
+//   brake(RMC1,RMC2, REN);  
+
+  
+// 	int newState;
+// 	initialize();
+//     //Perform learning trials starting at all initial states.
+  
+//   for (int k = 0; k < sizeof(initialStates) / sizeof(int); k++){
+//         episode(k);
+//   }
+
+//   Serial.println("done");
+
+//   //Print out Q matrix.
+//   for(int i = 0; i < qSize; i++){
+//     for(int j = 0; j < AAction; j++){
+//       Serial.print(Q[i][j]);
+//       if(j < qSize - 1){
+//         Serial.print(",");
+//       }
+//     } // j
+//       Serial.println();
+//   } // i
+//   Serial.println();
+// }
+
+// void loop(){ //maximum에 따른 최종값을 실행
+//   forward();
+//   int currentState = nowstates();
+//   int newState;
+// 	//Perform tests, starting at all initial states.
+// 	for (int i = 0; i < 9; i++) {//state마다 작동
+//         currentState = i;
+//         int newState = 0;
+//         newState = maximum(currentState, 1);//최선의 값
+//         Serial.print(currentState);
+//         currentState = newState;
+//         activateAction(currentState); // <- 동작
+//         delay(1000);
+//         Serial.println("");
+//     } // i  
+// }
+
+// void initialize(){//Q테이블 초기화
+// 	//srand((unsigned)time(0));
+//     for(int i = 0; i < qSize; i++){ // <- State
+//         for(int j = 0; j <qSize; j++){// <- Action
+//             Q[i][j] = 0;
+// 		} // j
+// 	} // i
+// }
+
+void episode(int initialState){//state에 대해    <-?????? 한 루프에 대한 끝 지점을 어떻게 집어넣지?// 각상황에 대해서
+  for (int i = 0; i < 10000; i++) {
+        chooseAnAction(initialState);
+  }
 }
 
-//////////////Q러닝////////////////////
-#define qSize 9
-#define AAction 4
-const double mygamma = 0.8;
-const int iterations = 10;
-int initialStates[qSize] = {0, 1, 2, 3, 4, 5, 6, 7, 8};//<-상태를 항상 받는걸로 하자. 
-//int States = 0; //<-현재 state?
-// 0 장애물 감지x
-// 1 장애물 감지 후(기준점 30cm) -30 
-// 2 -20
-// 3 -10
-// 4 사이
-// 5  10
-// 6  20
-// 7  30
-// 8 장애물과의 거리(10cm이하)
-
-// 이걸 state에 따른 값이라고 해야할텐데.
-//Action 0. 좌회전, 1. 우회전, 2. 전진, 3. 후진
-int R[qSize][AAction] =  {{1, 1, 10, -3},
-                  			{-3, 3, -3, 0},
-                  			{-2, 2, -3, 0},
-                  			{-1, 1, -3, 0},
-                  			{0, 0, -3, 0},
-                  			{1, -1, -3, 0},
-                  			{2, -2, -3, 0},
-                  			{3, -3, -3, 0},
-                  			{-1, -1, -10, 3}
-                  			};
-
-int Q[qSize][qSize];
-int currentState;
-
-void episode(int initialState);
-void chooseAnAction();
-int getRandomAction(int upperBound, int lowerBound);
-void initialize();
-int maximum(int state, int returnIndexOnly);
-int reward(int action);
-
-void setup(){
-	int newState;
-	initialize();
-    //Perform learning trials starting at all initial states.
-    for(int j = 0; j < iterations/*10*/; j++){
-        for(int i = 0; i < qSize; i++){
-            episode(initialStates[i]); //<-? 에피소드를 setup에서 Q를 완성시키는건가?
-		} // i
-	} // j
-
-  //Print out Q matrix.
-  for(int i = 0; i <= (qSize - 1); i++){
-    for(int j = 0; j <= (qSize - 1); j++){
-      Serial.print(Q[i][j]);
-      if(j < qSize - 1){
-        Serial.print(",");
-      }
-    } // j
-      Serial.println();
-  } // i
-  Serial.println();
-}
-
-void loop(){ //maximum에 따른 최종값을 실행
-  forward();
-  int primeState = nowstates();
-  int newState; // <-?
-	//Perform tests, starting at all initial states.
-	for(int i = 0; i < qSize; i++){
-    currentState = State;
-    newState = 0;
-		do {
-          newState = maximum(currentState, 1);
-          // Serial.print(currentState);
-          // Serial.print("->");
-          currentState = newState;
-          activateAction(currentState); // <- 동작?
-        } while(currentState < AAction); //<- 마지막 기준점을 어떻게 설정하지?
-    // Serial.println(5);
-	} // i  
-}
-
-void initialize(){//Q테이블 초기화
-	//srand((unsigned)time(0));
-    for(int i = 0; i < qSize; i++){ // <- State
-        for(int j = 0; j <qSize; j++){// <- Action
-            Q[i][j] = 0;
-		} // j
-	} // i
-}
-
-void episode(int initialState){//초기 state에 대해    <-?????? 한 루프에 대한 끝 지점을 어떻게 집어넣지?// 각상황에 대해서
-    currentState = initialState;
-    //Travel from state to state until goal state is reached. goal state == s[0]
-	do {//<- 왜 있는거지?//한 상황에 대해서 끝날 때 까지 실행한다. != 현 상태에서 계속 R값을 준다.
-      chooseAnAction();//<- 여기서 currentState값이 바뀐다. 따라서 State가 0일 때 끝내는건 어떨까
-	} while(currentState == 0);//전진 일 때 실행?
-
-    //When currentState = 0, run through the set once more to
-    //for convergence.
-  for(int i = 0; i < qSize; i++){//currentState마다 R값 Q에 집어넣는다.
-    chooseAnAction();
-  } // i
-}
-
-void chooseAnAction(){//방을 옮겼다는 느낌? //1회씩 진행된 값이 축적되어 Q값 쌓기
+void chooseAnAction(int shostate){//방을 옮겼다는 느낌? //1회씩 진행된 값이 축적되어 Q값 쌓기
 	int possibleAction;
 
   //Randomly choose a possible action connected to the current state.
-  possibleAction = getRandomAction(AAction, 0);//랜덤에서 R이 0보다 큰 Action을 가져와
-
-	if(R[currentState][possibleAction] >= 0){ //<- R 값에서 +값이면 Q테이블에 적용 
-    Q[currentState][possibleAction] = reward(possibleAction);
-    currentState = possibleAction;
-    activateAction(currentState);//0보다 큰 값을 실행
-	}
-}
-
-int getRandomAction(int upperBound, int lowerBound){//최대 - 최소//upperCound == qsize == 9, lowerBound == 0 // 1초당 진행 (1회씩 진행)
-	int action;
-	int choiceIsValid = 0;
-
-  //Randomly choose a possible action connected to the current state.
-  do {
-    //srand((unsigned)time(NULL))
-    //Get a random value between 0 and 4.
-    action = lowerBound + rand() % AAction;
-    //activateAction(action);
-    //delay(100); //<-여기서 문제가 생기나?
-    if(R[currentState][action] >= 0){//<-R테이블에서 0 이상이면 break
-      choiceIsValid = 1;
+  possibleAction = getRandomAction(AAction, 0,shostate);//랜덤에서 R이 0보다 큰 Action을 가져와
+	if (R[shostate][possibleAction] >= 0) { //<- R 값에서 +값이면 Q테이블에 적용 
+        Q[shostate][possibleAction] = reward(shostate, possibleAction);
+        // currentState = possibleAction;
+        // activateAction(currentState);//0보다 큰 값을 실행
     }
-  } while(choiceIsValid == 0);
-  return action;
 }
 
-// int nowstates(){
-//   int Fobstacle, Zobs;
-//   Fobstacle, Zobs= obscheck();
-//   if(Fobstacle<=30){
-//     if(Zobs<= -40){
+int getRandomAction(int upperBound, int lowerBound, int gettState){//최대 - 최소//upperCound == qsize == 9, lowerBound == 0 // 1초당 진행 (1회씩 진행)
+	int action;
+    int choiceIsValid = 0;
+    //Randomly choose a possible action connected to the current state.
+    do {
+        //Get a random value between 0 and 4.
+        action = lowerBound + rand() % AAction;
+        //activateAction(action);   
+        //delay(100); //<-여기서 문제가 생기나?
+        if (R[gettState][action] >= 0) {//<-R테이블에서 0 이상이면 break
+            break;
+        }
+    } while (choiceIsValid == 0);
+    return action;
+}
 
-//     }
-//   }
+// 0 장애물 감지x
+// 1 장애물 감지 후(기준점 30cm) -30 미만
+// 2 -30~-20
+// 3 -20~-10
+// 4 -10~10
+// 5  10~20
+// 6  20~30
+// 7  30 초과
+// 8 장애물과의 거리(10cm이하)
 
-//   return numstate
-// }
+int nowstates() {
+  int Fobstacle, Zobs, numstate;
+  Fobstacle = obscheck(0);
+  Zobs = obscheck(1);
+    if (Fobstacle < 10) {
+        numstate = 8;
+    }
+    else if (Fobstacle > 30) {
+        numstate = 0;
+    }
+    else {
+        if (Zobs > -30) {
+            numstate = 2;
+            if (Zobs >= -20) {
+                numstate = 3;
+                if (Zobs >= -10) {
+                    numstate = 4;
+                    if (Zobs >= 10) {
+                        numstate = 5;
+                        if (Zobs >= 20) {
+                            numstate = 6;
+                            if (Zobs >= 30) {
+                                numstate = 7;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            numstate = 1;
+        }
+    }
+    return numstate;
+}
 
-void obscheck(){
-  int Fobs, Lobs, Robs, Z;
-  Fobs = checkUlt(90)//in servo pos;
-  Robs = checkUlt(60)//in servo pos;
-  Lobs = checkUlt(120)//in servo pos;
+int obscheck(int need){
+  int Fobs= 0, Lobs = 0, Robs = 0, Z = 0;
+  Fobs = readUlt(90);//in servo pos;
+  delay(300);
+  Robs = readUlt(60);//in servo pos;
+  delay(300);
+  Lobs = readUlt(120);//in servo pos;
+  delay(300);
+  myServo.write(90);
+  
   Z = Lobs-Robs;
-  return Fobs, Z;
+  if(need == 0){
+    return Fobs;
+  }else{
+    return Z;
+  }
 }
 
 void activateAction(int Actnum){ //Action 0. 좌회전, 1. 우회전, 2. 전진, 3. 후진
@@ -325,7 +459,7 @@ void activateAction(int Actnum){ //Action 0. 좌회전, 1. 우회전, 2. 전진,
     rightturn();
   }
   else if(Actnum == 2){
-    forward();
+    go();
   }
   else {
     back();
@@ -341,7 +475,7 @@ int maximum(int state, int returnIndexOnly){
   winner = 0;  
   do {
       foundNewWinner = 0;
-      for(int i = 0; i < qSize; i++){//state에 대한 최고값 추출
+      for(int i = 0; i < AAction; i++){//state에 대한 최고값 추출
         if((i < winner) || (i > winner)){     //Avoid self-comparison. // i != winner일 때
           if(Q[state][i] > Q[state][winner]){
             winner = i;
@@ -363,15 +497,43 @@ int maximum(int state, int returnIndexOnly){
 	}
 }
 
-// float reward(int action){
+// double reward(int action){
 //     return R[currentState][action] + (mygamma * maximum(action, 0));
 // }
 
-int reward(int action){//축적된 R과, maximum에서의 값 == R + Q(g*maximum)
-    return R[currentState][action] + (mygamma * maximum(action, 0));
+// int reward(int action){//축적된 R과, maximum에서의 값 == R + Q(g*maximum)
+//     return R[currentState][action] + (mygamma * maximum(action, 0));
+// }
+
+float reward(int RcurrentState,int action) {//축적된 R과, maximum에서의 값 == R + Q(g*maximum)
+    return R[RcurrentState][action] + (mygamma * maximum(action, 0));
+}
+
+void initialize(){//Q테이블 초기화
+	//srand((unsigned)time(0));
+    for(int i = 0; i < qSize; i++){ // <- State
+        for(int j = 0; j <qSize; j++){// <- Action
+            Q[i][j] = 0;
+		} // j
+	} // i
 }
 
 
-
+void loop(){ //maximum에 따른 최종값을 실행
+  go();
+  int currentState = nowstates();
+  int newState;
+	//Perform tests, starting at all initial states.
+	for (int i = 0; i < 9; i++) {//state마다 작동
+        currentState = i;
+        int newState = 0;
+        newState = maximum(currentState, 1);//최선의 값
+        Serial.print(currentState);
+        currentState = newState;
+        activateAction(currentState); // <- 동작
+        delay(1000);
+        Serial.println("");
+    } // i  
+}
 
 
